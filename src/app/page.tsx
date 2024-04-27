a4be6cd463657b1741fc6273c2b6fe824a0c2561
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-26 23:49:54 -0400

    t3gallery - Scaffold Basic UI - Gap and Tailwind Body Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ const mockImages = mockUrls.map((url, index) => ({
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap gap-4">
         {mockImages.map((image) => (
-          <div key={image.id} className="w-1/2 p-4">
+          <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
           </div>
         ))}
