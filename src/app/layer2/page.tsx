3835f4a71ab89e6cb9fe015139191a62fe4bb11d
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-27 00:08:27 -0400

    t3gallery - Scaffold Basic UI - Fixed Issued Where the Spread Syntax was Breaking

--- src/app/layer2/page.tsx
+++ src/app/layer2/page.tsx
@@ -1,1 +1,7 @@
-export default
+export default function Layer2Page() {
+  return (
+    <main className="">
+      <h1>Page for layout 2</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[mockImages, ...mockImages, ...mockImages].map((image) => (
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
           </div>
