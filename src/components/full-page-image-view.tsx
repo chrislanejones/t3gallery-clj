c432f21e25048063be6a2c8c64771f1e66a07a70
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-09 04:16:45 -0400

    t3gallery - Routing/image - Model - More Sidebar Info and Padding

--- src/components/full-page-image-view.tsx
+++ src/components/full-page-image-view.tsx
@@ -1,14 +1,21 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0">
       <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
-      <div className="flex-shink-0 flex w-48 flex-col border-l">
-        <div className="p-2 text-xl font-bold">{image.name}</div>
+      <div className="flex-shink-0 w-100 flex flex-col gap-2 border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col p-2 text-center">
+          <span>Uploaded By:</span>
+          {uploaderInfo.fullName}
+        </div>
       </div>
     </div>
   );
