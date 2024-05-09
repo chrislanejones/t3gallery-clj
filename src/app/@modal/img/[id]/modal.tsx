e23f5d87377ef9b8bc123f87a6f9d10a6db1cf8e
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-08 22:48:23 -0400

    t3gallery - Routing/image - Model - More Cleanup on Modal

--- src/app/@modal/img/[id]/modal.tsx
+++ src/app/@modal/img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="absolute h-screen w-screen bg-black/90"
+      className="absolute h-screen w-screen bg-black/90 text-white"
       onClose={onDismiss}
     >
       {children}
--- src/components/full-page-image-view.tsx
+++ src/components/full-page-image-view.tsx
@@ -3,11 +3,11 @@ import { getImage } from "~/server/queries";
 export async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <div className="flex h-full w-full min-w-0 bg-green-500">
-      <div className="flex-shrink">
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
-      <div className="flex-shink-0 flex w-48 flex-col">
+      <div className="flex-shink-0 flex w-48 flex-col border-l">
         <div className="p-2 text-xl font-bold">{image.name}</div>
       </div>
     </div>
