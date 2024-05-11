f81dd36aa2f7d93b1b7527e0c4f1363d62ccbcf3
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-10 23:28:50 -0400

    t3gallery - Fixing the Upload Button

---
+++ src/_components/simple-upload-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useUploadThing } from "~/utils/uploadthing";
+
+// inferred input off useUploadThing
+type Input = Parameters<typeof useUploadThing>;
+
+const useUploadThingInputProps = (...args: Input) => {
+  const $ut = useUploadThing(...args);
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    const result = await $ut.startUpload(selectedFiles);
+
+    console.log("uploaded files", result);
+    // TODO: persist result in state maybe?
+  };
+
+  return {
+    inputProps: {
+      onChange,
+      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+      accept: "image/*",
+    },
+    isUploading: $ut.isUploading,
+  };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ async function Images() {
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[...images, ...images, ...images, ...images].map((image) => (
+      {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
